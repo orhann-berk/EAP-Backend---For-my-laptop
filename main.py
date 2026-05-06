7c6d7b3dce684fc2f6b17d9c93e5b89723dff4a8
orhann-berk <orhan.berk.durak@motopp.nl>
2026-05-06 17:22:31 +0200

    changed base metadata

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from database.database import engine
-from database import models
 from routers import registration
+from database.database import Base
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app.include_router(registration.router)
